893f9468662ad807213a3a57e3fb7a4b5aa5b3b3
godo <godo@sboa1045.fnet.local>
2019-09-27 15:17:24 +0900

    create test environment.

---
+++ test/mrkt/ror/src/AppClient.cpp
@@ -0,0 +1,57 @@
+Function '_GLOBAL__sub_I__ZN9AppClientC2EPN14thomsonreuters3ema6access11OmmConsumerEPKc'
+Lines executed:100.00% of 1
+
+Function '__static_initialization_and_destruction_0(int, int)'
+Lines executed:100.00% of 100
+
+Function 'AppClient::getServiceName()'
+Lines executed:100.00% of 2
+
+Function 'AppClient::request(thomsonreuters::ema::access::ReqMsg&)'
+Lines executed:38.89% of 18
+
+Function 'AppClient::errorNotification(char const*, thomsonreuters::ema::access::OmmState::DataState, unsigned char, char const*)'
+Lines executed:0.00% of 4
+
+Function 'AppClient::decode(thomsonreuters::ema::access::EmaString const&, thomsonreuters::ema::access::OmmState const*, thomsonreuters::ema::access::FieldList const&)'
+Lines executed:0.00% of 8
+
+Function 'AppClient::decode(thomsonreuters::ema::access::Msg const&, thomsonreuters::ema::access::OmmState const*, bool)'
+Lines executed:62.50% of 8
+
+Function 'AppClient::onStatusMsg(thomsonreuters::ema::access::StatusMsg const&, thomsonreuters::ema::access::OmmConsumerEvent const&)'
+Lines executed:75.00% of 20
+
+Function 'AppClient::onUpdateMsg(thomsonreuters::ema::access::UpdateMsg const&, thomsonreuters::ema::access::OmmConsumerEvent const&)'
+Lines executed:0.00% of 14
+
+Function 'AppClient::onRefreshMsg(thomsonreuters::ema::access::RefreshMsg const&, thomsonreuters::ema::access::OmmConsumerEvent const&)'
+Lines executed:68.75% of 16
+
+Function 'AppClient::getStatus()'
+Lines executed:0.00% of 2
+
+Function 'AppClient::stop()'
+Lines executed:0.00% of 7
+
+Function 'AppClient::~AppClient()'
+Lines executed:0.00% of 2
+
+Function 'AppClient::~AppClient()'
+Lines executed:99.99% of 2
+
+Function 'AppClient::AppClient(thomsonreuters::ema::access::OmmConsumer*, char const*)'
+Lines executed:100.00% of 4
+
+Function 'AppClient::c_str() const'
+Lines executed:100.00% of 2
+
+File 'AppClient.cpp'
+Lines executed:44.44% of 108
+
+File '../../Elektron-SDK1.2.1.linux.rrg/Cpp-C/Ema/Src/Rdm/Include/EmaRdm.h'
+Lines executed:100.00% of 99
+
+File 'AppClient.h'
+Lines executed:100.00% of 2
+
---
+++ test/mrkt/ror/src/RorManager.cpp
@@ -0,0 +1,32 @@
+Function '_GLOBAL__sub_I__ZN10RorManagerC2Ev'
+Lines executed:100.00% of 1
+
+Function '__static_initialization_and_destruction_0(int, int)'
+Lines executed:100.00% of 100
+
+Function 'RorManager::snapshot(InternalInfoTable_*)'
+Lines executed:54.55% of 11
+
+Function 'RorManager::~RorManager()'
+Lines executed:100.00% of 2
+
+Function 'RorManager::~RorManager()'
+No executable lines
+
+Function 'RorManager::RorManager()'
+Lines executed:100.00% of 2
+
+Function 'Manager::dictionary(char**, unsigned long)'
+Lines executed:0.00% of 2
+
+Function 'Manager::snapshot()'
+Lines executed:0.00% of 2
+
+File 'RorManager.cpp'
+Lines executed:68.75% of 16
+
+File '../../Elektron-SDK1.2.1.linux.rrg/Cpp-C/Ema/Src/Rdm/Include/EmaRdm.h'
+Lines executed:100.00% of 99
+
+File '../../reuters/ema/impl/Manager.h'
+Lines executed:0.00% of 4
